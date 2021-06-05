e74b9a51fdb24e047302bcc9dc5c24c55445d0dc
BenRozilio <63303190+benrozilio@users.noreply.github.com>
2021-06-05 14:11:00 +0300

    get league teams

--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -1,34 +1,47 @@
 const axios = require("axios");
 const LEAGUE_ID = 271;
+const api_domain = "https://soccer.sportmonks.com/api/v2.0";
+
 
 async function getLeagueDetails() {
-  const league = await axios.get(
-    `https://soccer.sportmonks.com/api/v2.0/leagues/${LEAGUE_ID}`,
-    {
-      params: {
-        include: "season",
-        api_token: process.env.api_token,
-      },
-    }
-  );
-  console.log(league);
-
   
-  const stage = await axios.get(
-    `https://soccer.sportmonks.com/api/v2.0/stages/${league.data.data.current_stage_id}`,
-    {
-      params: {
-        api_token: process.env.api_token,
-      },
-    }
-  );
-  
-  return {
-    league_name: league.data.data.name,
-    current_season_name: league.data.data.season.data.name,
-    current_stage_name: stage.data.data.name,
-    // next game details should come from DB
-  };
-  
+  const res = await axios.get(`${api_domain}/leagues/${LEAGUE_ID}`, {
+    params: {
+      include: 'season',
+      api_token: process.env.api_token,
+    },
+  });
+  return res.data;
 }
+
+async function getTeams(sorted){
+  
+  league = await getLeagueDetails();
+  if (!league){ return }
+  let id =  league.data.season.data.id;
+  const teams = getSeasonTeams(id);
+  if ( sorted == 'false' )
+    return teams;
+  
+  teams = teams.sort(function(team1, team2){
+    if(team2.name < team1.name) { return -1; }
+    if(team2.name > team1.name) { return 1; }
+    return 0;
+})
+return teams;
+}
+
+async function getSeasonTeams(seasonId){
+  const res = await axios.get(`${api_domain}/teams/season/${seasonId}`, {
+    params: {
+      api_token: process.env.api_token,
+    },
+  });
+  return res.data.data;
+}
+
+
+
+
+exports.getTeams = getTeams;
 exports.getLeagueDetails = getLeagueDetails;
